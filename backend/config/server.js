7914026f8737064738b143c7e918ca1880bb18fc
John Cornyn <JMIHC@users.noreply.github.com>
2021-02-14 21:43:42 -0800

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c19e747a39e864587f647797b79367f6'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b9e76dab-a31e-41f4-82ef-2a3023e20336'
+};
